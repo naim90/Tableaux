8d6ad76b3b36085f0396add84ec1d80509b98f64
naim90 <naim.elmoudni@gmail.com>
2021-09-22 18:37:38 +0200

    Concaténer des tableaux

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tableaux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] tableau1 = new char[3];
+            char[] tableau2 = new char[3];
+            char[] tableau3 = new char[tableau1.Length+tableau2.Length];
+
+            tableau1[0] = 'b';
+            tableau1[1] = 'u';
+            tableau1[2] = 'e';
+            tableau2[0] = 'n';
+            tableau2[1] = 'o';
+            tableau2[2] = '!';
+
+
+            tableau1.CopyTo(tableau3, 0);
+            //tableau2.CopyTo(tableau3, 3);
+
+
+            Array.ConstrainedCopy(tableau2, 0, tableau3, tableau1.Length, tableau2.Length);
+
+            Console.Write(tableau3);
+
+
+        }
+    }
+}
